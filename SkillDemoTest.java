c08337fde3d290098c7c24eca161f34dd2172749
Justin Sin <jusin@ucsd.edu>
2022-02-04 13:51:40 -0800

    Failing Test + Lib

---
+++ SkillDemoTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemoTest {
+    @Test
+    public void multiplication() {
+        assertEquals(6, SkillDemo.multiplication(2, 4));
+    }
+}
